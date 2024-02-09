9ba27652e8aa3bfef3e7e5f32615e303b333705b
Nicolas Deschildre <nicolas@deschildre.fr>
2024-02-09 11:24:56 +0100

    Bump web3protocol to support the 2 new candidate ERCs for the resource request mode.

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,22 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // We want support of top level await for brotli-wasm
+  build: {
+    target: 'esnext',
+  },
+  // Yarn run dev: it won't work with the loading of the wasm file
+  optimizeDeps: {
+     exclude: ['brotli-wasm'],
+     esbuildOptions: {
+      target: 'esnext',
+     },
+  },
+  // Local dev: Yarn run dev with npm link
+  // server: {
+  //   fs: {
+  //     allow: ['.', '/home/nand/Code/web3protocol/web3protocol-js'],
+  //   },
+  // },
 })
